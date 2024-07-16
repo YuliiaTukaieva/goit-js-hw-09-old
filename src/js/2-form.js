8f7c0b117b493eec1839012c483faacbeb4e430e
YuliiaTukaieva <jul.tukaeva@gmail.com>
2024-07-16 03:23:53 +0300

    done

---
+++ src/js/2-form.js
@@ -0,0 +1,78 @@
+let formData = {
+    email: "",
+    message: ""
+  };
+
+const form = document.querySelector(".feedback-form");
+
+populateForm();
+
+form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("input", handleFormInput);
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+
+//   localStorage.removeItem('feedback-form-state');
+
+//   event.currentTarget.reset();
+// }
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+    event.currentTarget.reset();
+    
+    if (!formData.email || !formData.message) {
+      alert('Fill please all fields');
+      return;
+    }
+}
+
+function handleFormInput(event) {
+  const value = event.target.value;
+  const key = event.target.name;
+
+//   let savedFeedbackData = {};
+
+  try {
+    formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  if (formData) {
+    formData[key] = value;
+  } else {
+    formData = {
+      [key]: value,
+    };
+  }
+
+  try {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+function populateForm() {
+//   let savedFeedbackData = {};
+
+  try {
+    formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  if (!formData) {
+    return;
+  }
+
+  for (const key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
